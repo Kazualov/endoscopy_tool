b8e2205ced29077f70d887a822fede10ffcd1b08
ruzhbaykaa <priivan2005@gmail.com>
2025-06-17 22:39:00 +0300

    Start page update

--- lib/pages/patients.dart
+++ lib/pages/patients.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
 
-void main() {
-  runApp(EndoscopistApp());
-}
-
 class EndoscopistApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
