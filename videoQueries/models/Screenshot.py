9a54e9f8b2f3695f8d25925e28bbe54c764cd956
Dan <kite@MacBook-Air-Danil.local>
2025-06-19 02:26:30 +0300

    Added the ability to create checkups, upload videos and screenshots for checkups.

---
+++ videoQueries/models/Screenshot.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, func
+from videoQueries.database import Base
+
+class Screenshot(Base):
+    __tablename__ = "screenshots"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    exam_id = Column(String, ForeignKey("examinations.id"), nullable=False)  # изменено
+    filename = Column(String, nullable=False)
+    file_path = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
