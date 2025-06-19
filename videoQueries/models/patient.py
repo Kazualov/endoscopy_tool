d86792ea1c6fb53273a844a7fcf4ca01390f15a3
Dan <kite@MacBook-Air-Danil.local>
2025-06-19 10:10:07 +0300

    Updated attributes of patients

--- videoQueries/models/patient.py
+++ videoQueries/models/patient.py
@@ -1,14 +1,18 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Date
 from videoQueries.database import Base
 from sqlalchemy.orm import relationship
+
 
 
 class Patient(Base):
     __tablename__ = "patients"
 
     id = Column(String, primary_key=True, index=True)
-    name = Column(String)
-    age = Column(String)
-    gender = Column(String)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    middlename = Column(String, nullable=True)
+    birthday = Column(Date, nullable=True)
+    male = Column(String, nullable=True)
     examinations = relationship("Examination", back_populates="patient")
+
 
