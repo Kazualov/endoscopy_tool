e427d2cb56499c741c4200a0c321740a4b2e3406
Dan <kite@MacBook-Air-Danil.local>
2025-06-20 12:48:51 +0300

    Corrected patient additions (accepts only id) based on feedback

--- videoQueries/models/patient.py
+++ videoQueries/models/patient.py
@@ -9,10 +9,6 @@ class Patient(Base):
 
     id = Column(String, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    middlename = Column(String, nullable=True)
-    birthday = Column(Date, nullable=True)
-    gender = Column(String, nullable=True)
     examinations = relationship("Examination", back_populates="patient")
 
 
