7e50de9ab289fe9d373a4c68da6a4250988d3ca0
azamat <azamot1301@gmail.com>
2025-06-22 18:46:05 +0300

    feat: added database for backend

--- videoQueries/models/Examination.py
+++ videoQueries/models/Examination.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, String, ForeignKey, DateTime, func
 from sqlalchemy.orm import relationship
 from videoQueries.database import Base  # Импорт единого Base
 
+
 class Examination(Base):
     __tablename__ = "examinations"
     id = Column(String, primary_key=True, index=True)
