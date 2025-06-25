558c1a014aa0371ed746cd13115fbbe3768fd87f
Dan <kite@MacBook-Air-Danil.local>
2025-06-25 11:57:41 +0300

    feat: Added endpoints for Examination (creation, deletion, upload video to examination, get) and endpoints for Screenshots (upload, get screenshot, get info)

--- videoQueries/routers/Screenshots.py
+++ videoQueries/routers/Screenshots.py
@@ -3,7 +3,7 @@ import shutil
 from pathlib import Path
 from typing import List
 
-from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
+from fastapi import APIRouter, UploadFile, File, Depends, HTTPException, Form
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 
@@ -30,6 +30,7 @@ os.makedirs(SCREENSHOT_DIR, exist_ok=True)
 @router.post("/exams/{exam_id}/upload_screenshot/")
 async def upload_screenshot(
     exam_id: str,
+    timestamp_in_video: float = Form(...),
     file: UploadFile = File(...),
     db: Session = Depends(get_db)
 ):
@@ -40,7 +41,8 @@ async def upload_screenshot(
     screenshot = Screenshot(
         exam_id=exam_id,
         filename=file.filename,
-        file_path=""
+        file_path="",
+        timestamp_in_video = timestamp_in_video
     )
     db.add(screenshot)
     db.flush()
@@ -55,7 +57,7 @@ async def upload_screenshot(
     screenshot.filename = filename
     db.commit()
 
-    return {"screenshot_id": screenshot.id, "filename": filename}
+    return {"screenshot_id": screenshot.id, "time": timestamp_in_video}
 
 
 @router.get("/exams/{exam_id}/screenshots", response_model=List[ScreenshotResponse])
@@ -67,7 +69,7 @@ def get_screenshots(exam_id: str, db: Session = Depends(get_db)):
     screenshots = (
         db.query(Screenshot)
         .filter(Screenshot.exam_id == exam_id)
-        .order_by(Screenshot.created_at)
+        .order_by(Screenshot.timestamp_in_video)
         .all()
     )
 
@@ -79,7 +81,7 @@ def get_screenshots(exam_id: str, db: Session = Depends(get_db)):
                 "exam_id": shot.exam_id,
                 "filename": shot.filename,
                 "file_path": shot.file_path,
-                "created_at": shot.created_at
+                "time": shot.timestamp_in_video
             })
 
     return result
