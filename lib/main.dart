878cc0d74e1988d1c1db78381b160be3b1cc3059
Eeeee13 <egornava.13@gmail.com>
2025-06-25 09:52:53 +0300

    integrate screenShotsEditor.dart into progect

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,42 @@
 import 'dart:io';
+import 'package:endoscopy_tool/pages/start_page.dart';
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
-import 'package:path_provider/path_provider.dart';
 
-import 'package:endoscopy_tool/pages/screenShotsEditor.dart';
+import 'package:endoscopy_tool/pages/patient_library.dart';
 
-void main() {
+Future<void> launchMyExe() async {
+  final exePath = 'endoscopy_tool/windows/runner/assets/backend_launcher/main.exe'; // relative to your app executable
+
+  try {
+    final process = await Process.start(exePath, []);
+    // Optional: capture output
+    process.stdout.transform(SystemEncoding().decoder).listen(print);
+    process.stderr.transform(SystemEncoding().decoder).listen(print);
+  } catch (e) {
+    print('Failed to launch exe: $e');
+  }
+}
+
+void main(){
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
+  //
+  //launchMyExe();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   MyApp({super.key});
 
-  final GlobalKey<ScreenshotEditorState> editorKey = GlobalKey<ScreenshotEditorState>();
-
   @override
-  Widget build(BuildContext context) {
-    final screenshotFile = File("/Users/egornava/Pictures/screenshots/photo/Screenshot 2025-05-21 at 18.52.41.png");
+  Widget build(BuildContext context){
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Stack(
-          children: [
-            ScreenshotEditor(
-              key: editorKey,
-              screenshot: FileImage(screenshotFile),
-              otherScreenshots: [FileImage(screenshotFile)],
-            ),
-            Positioned(
-              bottom: 20,
-              right: 20,
-              child: FloatingActionButton(
-                onPressed: () async {
-                  final path = await editorKey.currentState?.save();
-                  if (path != null) {
-                    print('Сохранено в: $path');
-                    ScaffoldMessenger.of(editorKey.currentContext!).showSnackBar(
-                      SnackBar(content: Text('Сохранено: $path')),
-                    );
-                  }
-                },
-                child: const Icon(Icons.save),
-              ),
-            ),
-          ],
-        ),
-      ),
+      //home: MainPage(videoPath: '/Users/ivan/Documents/Videos for project/videos/Bad Piggies Soundtrack | Building Contraptions | ABFT.mp4')
+      home: EndoscopistApp(),
+      //home: StartPage()
     );
   }
 }
