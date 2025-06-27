85ad7192a32283b0d5ffc573e2aec9b5d2804e84
azamat <azamot1301@gmail.com>
2025-06-27 18:52:48 +0300

    fix: fixed errors in the code

--- videoQueries/main.py
+++ videoQueries/main.py
@@ -36,5 +36,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
