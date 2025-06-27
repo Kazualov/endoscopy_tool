5fae4760c6f73f9f5f88118ada95fcecf837c369
Eeeee13 <egornava.13@gmail.com>
2025-06-28 01:13:14 +0300

    not working

--- lib/pages/patient_library.dart
+++ lib/pages/patient_library.dart
@@ -66,7 +66,9 @@ class Patient {
 
 // Сервис для работы с API
 class ApiService {
-  static const String baseUrl = 'http://127.0.0.1:8000'; 
+  static const String baseUrl = 'http://127.0.0.1:8000';
+  final http.Client client;
+  ApiService({http.Client? client}) : client = client ?? http.Client();
 
 
   static Future<List<Examination>> getExamination() async {
---
+++ lib/test/api_service_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../pages/patient_library.dart';
+
+// ✅ СНАЧАЛА аннотация
+@GenerateMocks([http.Client])
+
+
+void main() {
+}
