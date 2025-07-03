92f83d9e15fdbfde97764a9b5290de61352e1daf
Dan <kite@MacBook-Air-Danil.local>
2025-07-03 23:48:51 +0300

    Returned Detection and Screenshot

---
+++ videoQueries/schemas/screenshots.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ScreenshotResponse(BaseModel):
+    screenshot_id: int
+    exam_id: str  # заменено с video_id
+    filename: str
+    file_path: str
+    created_at: datetime
+    annotated_filename: str | None = None
+    annotated_file_path: str | None = None
+    class Config:
+        from_attributes = True
